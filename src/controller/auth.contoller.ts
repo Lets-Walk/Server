d8eafd2e22b4a35623104dd700846db34fd0b8bd
jjonyo <jonghyuk96@cau.ac.kr>
2021-09-30 15:12:12 +0900

    Add auth-signup, auth-email

--- src/controller/auth.contoller.ts
+++ src/controller/auth.contoller.ts
@@ -1,13 +1,23 @@
 import express from 'express'
+
 import AuthService from '../service/auth.service'
 
 const authController = express.Router()
 
-authController.get('/login', (req, res) => {
-  res.send('hello')
-  //   const data = req.body
-  //   const result = AuthService.login(data)
-  //   return result
+authController.post('/sign-up', async (req, res) => {
+  const userData = req.body
+
+  res.send(await AuthService.signup(userData))
+})
+
+authController.post('/email', async (req, res) => {
+  const { email } = req.body
+  if (!email) {
+    res.status(400).send('email does not exist')
+    return
+  }
+  const code = await AuthService.email_validation(email)
+  res.send(code)
 })
 
 export default authController
--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,11 +1,38 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+
+import db from '../../models'
 
 class AuthService {
   constructor() {}
 
-  // async login(data) {
-  //   // code here
-  // }
+  async signup(userData) {
+    const { password } = userData
+    const hashPassword = await bcrypt.hash(password, 12)
+
+    try {
+      await db.User.create({
+        name: userData.name,
+        email: userData.email,
+        nickname: userData.nickname,
+        password: hashPassword,
+        CampusId: userData.campusId,
+      })
+      return { success: true }
+    } catch (err) {
+      console.error(err)
+      return {
+        success: false,
+        message: 'DB Create fail',
+      }
+    }
+  }
+
+  async email_validation(email) {
+    //TODO :: nodemail통해 mail인증 구현하기, 메일 validation
+    //일단은 인증코드로 0000 return 하는걸로
+    return { code: '0000' }
+  }
 }
 
 export default new AuthService()
