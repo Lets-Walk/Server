8d7c52b997ba73391ca7e081151328670ca86e98
jjjuurang <jjjuurang@naver.com>
2021-10-06 23:50:49 +0900

    all campus_list

--- constants/campus_list.ts
+++ constants/campus_list.ts
@@ -1,8 +1,414 @@
 export default [
   {
-    name: '중앙대학교',
-    domain: '@cau.ac.kr',
-    image: 'cau.png',
+    name: '가야대학교',
+    domain: '@kaya.ac.kr',
+    image: 'kaya.png',
+  },
+  {
+    name: '가천대학교',
+    domain: '@gachon.ac.kr',
+    image: 'gachon.png',
+  },
+  {
+    name: '가톨릭관동대학교',
+    domain: '@cku.ac.kr',
+    image: 'cku.png',
+  },
+  {
+    name: '가톨릭대학교',
+    domain: '@catholic.ac.kr',
+    image: 'catholic.png',
+  },
+  {
+    name: '감리교신학대학교',
+    domain: '@mtu.ac.kr',
+    image: 'mtu.png',
+  },
+  {
+    name: '강남대학교',
+    domain: '@kangnam.ac.kr',
+    image: 'kangnam.png',
+  },
+  {
+    name: '강릉원주대학교',
+    domain: '@gwnu.ac.kr',
+    image: 'gwnu.png',
+  },
+  {
+    name: '강원대학교',
+    domain: '@kangwon.ac.kr',
+    image: 'kangwon.png',
+  },
+  {
+    name: '건국대학교',
+    domain: '@kku.ac.kr',
+    image: 'kku.png',
+  },
+  {
+    name: '건양대학교',
+    domain: '@konyang.ac.kr',
+    image: 'konyang.png',
+  },
+  {
+    name: '경기대학교',
+    domain: '@kyonggi.ac.kr',
+    image: 'kyonggi.png',
+  },
+  {
+    name: '경남과학기술대학교',
+    domain: '@1gntech.ac.kr',
+    image: '1gntech.png',
+  },
+  {
+    name: '경남대학교',
+    domain: '@kyungnam.ac.kr',
+    image: 'kyungnam.png',
+  },
+  {
+    name: '경동대학교',
+    domain: '@kduniv.ac.kr',
+    image: 'kduniv.png',
+  },
+  {
+    name: '경복대학교',
+    domain: '@knu.ac.kr',
+    image: 'knu.png',
+  },
+  {
+    name: '경상국립대학교',
+    domain: '@newghgnu.ac.kr',
+    image: 'newghgnu.png',
+  },
+  {
+    name: '경성대학교',
+    domain: '@ks.ac.kr',
+    image: 'ks.png',
+  },
+  {
+    name: '경운대학교',
+    domain: '@ikw.ac.kr',
+    image: 'ikw.png',
+  },
+  {
+    name: '고려대학교',
+    domain: '@korea.ac.kr',
+    image: 'korea.png',
+  },
+  {
+    name: '경일대학교',
+    domain: '@kiu.ac.kr',
+    image: 'kiu.png',
+  },
+  {
+    name: '경주대학교',
+    domain: '@gu.ac.kr',
+    image: 'gu.png',
+  },
+  {
+    name: '경희대학교',
+    domain: '@khu.ac.kr',
+    image: 'khu.png',
+  },
+
+  {
+    name: '계명대학교',
+    domain: '@kmu.ac.kr',
+    image: 'kmu.png',
+  },
+  {
+    name: '고려대학교',
+    domain: '@korea.ac.kr',
+    image: 'korea.png',
+  },
+  {
+    name: '고려대학교세종캠퍼스',
+    domain: '@sejongkorea.ac.kr',
+    image: 'sejongkorea.png',
+  },
+  {
+    name: '고신대학교',
+    domain: '@kosin.ac.kr',
+    image: 'kosin.png',
+  },
+  {
+    name: '공주대학교',
+    domain: '@kongju.ac.kr',
+    image: 'kongju.png',
+  },
+  {
+    name: '광신대학교',
+    domain: '@kwangshin.ac.kr',
+    image: 'kwangshin.png',
+  },
+  {
+    name: '광운대학교',
+    domain: '@kw.ac.kr',
+    image: 'kw.png',
+  },
+  {
+    name: '광주가톨릭대학교',
+    domain: '@gjcatholic.ac.kr',
+    image: 'gjcatholic.png',
+  },
+  {
+    name: '광주과학기술원',
+    domain: '@gist.ac.kr',
+    image: 'gist.png',
+  },
+  {
+    name: '광주대학교',
+    domain: '@gwangju.ac.kr',
+    image: 'gwangju.png',
+  },
+  {
+    name: '광주여자대학교',
+    domain: '@kwu.ac.kr',
+    image: 'kwu.png',
+  },
+  {
+    name: '국민대학교',
+    domain: '@kookmin.ac.kr',
+    image: 'kookmin.png',
+  },
+  {
+    name: '군산대학교',
+    domain: '@kunsan.ac.kr',
+    image: 'kunsan.png',
+  },
+  {
+    name: '극동대학교',
+    domain: '@kdu.ac.kr',
+    image: 'kdu.png',
+  },
+  {
+    name: '금강대학교',
+    domain: '@ggu.ac.kr',
+    image: 'ggu.png',
+  },
+  {
+    name: '금오공과대학교',
+    domain: '@kumoh.ac.kr',
+    image: 'kumoh.png',
+  },
+  {
+    name: '김천대학교',
+    domain: '@gimcheon.ac.kr',
+    image: 'gimcheon.png',
+  },
+  {
+    name: '꽃동네대학교',
+    domain: '@kkot.ac.kr',
+    image: 'kkot.png',
+  },
+  {
+    name: '나사렛대학교',
+    domain: '@kornu.ac.kr',
+    image: 'kornu.png',
+  },
+  {
+    name: '남부대학교',
+    domain: '@nambu.ac.kr',
+    image: 'nambu.png',
+  },
+  {
+    name: '남서울대학교',
+    domain: '@nsu.ac.kr',
+    image: 'nsu.png',
+  },
+  {
+    name: '단국대학교',
+    domain: '@dankook.ac.kr',
+    image: 'dankook.png',
+  },
+  {
+    name: '대구가톨릭대학교',
+    domain: '@cu.ac.kr',
+    image: 'cu.png',
+  },
+  {
+    name: '대구경북과학기술원',
+    domain: '@dgist.ac.kr',
+    image: 'dgist.png',
+  },
+  {
+    name: '대구대학교',
+    domain: '@daegu.ac.kr',
+    image: 'daegu.png',
+  },
+  {
+    name: '대구예술대학교',
+    domain: '@dgau.ac.kr',
+    image: 'dgau.png',
+  },
+  {
+    name: '대구한의대학교',
+    domain: '@dhu.ac.kr',
+    image: 'dhu.png',
+  },
+  {
+    name: '대신대학교',
+    domain: '@daeshin.ac.kr',
+    image: 'daeshin.png',
+  },
+  {
+    name: '대전가톨릭대학교',
+    domain: '@dcatholic.ac.kr',
+    image: 'dcatholic.png',
+  },
+  {
+    name: '대전대학교',
+    domain: '@dju.ac.kr',
+    image: 'dju.png',
+  },
+  {
+    name: '대선신학대학교',
+    domain: '@daejeon.ac.kr',
+    image: 'daejeon.png',
+  },
+  {
+    name: '대진대학교',
+    domain: '@daejin.ac.kr',
+    image: 'daejin.png',
+  },
+  {
+    name: '덕성여자대학교',
+    domain: '@duksung.ac.kr',
+    image: 'duksung.png',
+  },
+  {
+    name: '동국대학교',
+    domain: '@donggukedu.ac.kr',
+    image: 'donggukedu.png',
+  },
+  {
+    name: '동덕여자대학교',
+    domain: '@dongduk.ac.kr',
+    image: 'dongduk.png',
+  },
+  {
+    name: '동명대학교',
+    domain: '@tu.ac.kr',
+    image: 'tu.png',
+  },
+  {
+    name: '동서대학교',
+    domain: '@dongseo.ac.kr',
+    image: 'dongseo.png',
+  },
+  {
+    name: '동신대학교',
+    domain: '@dsu.ac.kr',
+    image: 'dsu.png',
+  },
+  {
+    name: '동아대학교',
+    domain: '@donga.ac.kr',
+    image: 'donga.png',
+  },
+  {
+    name: '동양대학교',
+    domain: '@dyu.ac.kr',
+    image: 'dyu.png',
+  },
+  {
+    name: '동의대학교',
+    domain: '@deu.ac.kr',
+    image: 'deu.png',
+  },
+  {
+    name: '루터대학교',
+    domain: '@ltu.ac.kr',
+    image: 'ltu.png',
+  },
+  {
+    name: '명지대학교',
+    domain: '@mju.ac.kr',
+    image: 'mju.png',
+  },
+  {
+    name: '목원대학교',
+    domain: '@mokwon.ac.kr',
+    image: 'mokwon.png',
+  },
+  {
+    name: '목포가톨릭대학교',
+    domain: '@mcu.ac.kr',
+    image: 'mcu.png',
+  },
+  {
+    name: '목포대학교',
+    domain: '@mokpo.ac.kr',
+    image: 'mokpo.png',
+  },
+  {
+    name: '목포해양대학교',
+    domain: '@mmu.ac.kr',
+    image: 'mmu.png',
+  },
+  {
+    name: '배재대학교',
+    domain: '@pcu.ac.kr',
+    image: 'pcu.png',
+  },
+  {
+    name: '백석대학교',
+    domain: '@bu.ac.kr',
+    image: 'bu.png',
+  },
+  {
+    name: '부경대학교',
+    domain: '@pknu.ac.kr',
+    image: 'pknu.png',
+  },
+  {
+    name: '부산가톨릭대학교',
+    domain: '@cup.ac.kr',
+    image: 'cup.png',
+  },
+  {
+    name: '부산대학교',
+    domain: '@pusan.ac.kr',
+    image: 'pusan.png',
+  },
+  {
+    name: '부산외국어대학교',
+    domain: '@bufs.ac.kr',
+    image: 'bufs.png',
+  },
+  {
+    name: '부산장신대학교',
+    domain: '@bpu.ac.kr',
+    image: 'bpu.png',
+  },
+  {
+    name: '삼육대학교',
+    domain: '@syu.ac.kr',
+    image: 'syu.png',
+  },
+  {
+    name: '상명대학교',
+    domain: '@smu.ac.kr',
+    image: 'smu.png',
+  },
+  {
+    name: '상지대학교',
+    domain: '@sangji.ac.kr',
+    image: 'sangji.png',
+  },
+  {
+    name: '서강대학교',
+    domain: '@sogang.ac.kr',
+    image: 'sogang.png',
+  },
+  {
+    name: '서경대학교',
+    domain: '@skuniv.ac.kr',
+    image: 'skuniv.png',
+  },
+  {
+    name: '서울과학기술대학교',
+    domain: '@seoultech.ac.kr',
+    image: 'seoultech.png',
   },
   {
     name: '서울대학교',
@@ -10,8 +416,524 @@ export default [
     image: 'snu.png',
   },
   {
-    name: '고려대학교',
-    domain: '@korea.ac.kr',
-    image: 'korea.png',
+    name: '서울시립대학교',
+    domain: '@uos.ac.kr',
+    image: 'uos.png',
+  },
+  {
+    name: '서울신학대학교',
+    domain: '@stu.ac.kr',
+    image: 'stu.png',
+  },
+  {
+    name: '서울여자대학교',
+    domain: '@swu.ac.kr',
+    image: 'swu.png',
+  },
+  {
+    name: '서울장신대학교',
+    domain: '@sjs.ac.kr',
+    image: 'sjs.png',
+  },
+  {
+    name: '서울한영대학교',
+    domain: '@hytu.ac.kr',
+    image: 'hytu.png',
+  },
+  {
+    name: '서원대학교',
+    domain: '@seowon.ac.kr',
+    image: 'seowon.png',
+  },
+  {
+    name: '선문대학교',
+    domain: '@sunmoon.ac.kr',
+    image: 'sunmoon.png',
+  },
+  {
+    name: '성결대학교',
+    domain: '@sungkyuledu.ac.kr',
+    image: 'sungkyuledu.png',
+  },
+  {
+    name: '성공회대학교',
+    domain: '@skhu.ac.kr',
+    image: 'skhu.png',
+  },
+  {
+    name: '성균관대학교',
+    domain: '@skkuedu.ac.kr',
+    image: 'skkuedu.png',
+  },
+  {
+    name: '성신여자대학교',
+    domain: '@sungshin.ac.kr',
+    image: 'sungshin.png',
+  },
+  {
+    name: '세명대학교',
+    domain: '@semyung.ac.kr',
+    image: 'semyung.png',
+  },
+  {
+    name: '세종대학교',
+    domain: '@sejong.ac.kr',
+    image: 'sejong.png',
+  },
+  {
+    name: '세한대학교',
+    domain: '@sehan.ac.kr',
+    image: 'sehan.png',
+  },
+  {
+    name: '송원대학교',
+    domain: '@songwon.ac.kr',
+    image: 'songwon.png',
+  },
+  {
+    name: '수원가톨릭대학교',
+    domain: '@suwoncatholic.ac.kr',
+    image: 'suwoncatholic.png',
+  },
+  {
+    name: '수원대학교',
+    domain: '@suwon.ac.kr',
+    image: 'suwon.png',
+  },
+  {
+    name: '숙명여자대학교',
+    domain: '@sookmyung.ac.kr',
+    image: 'sookmyung.png',
+  },
+  {
+    name: '순천대학교',
+    domain: '@sunchon.ac.kr',
+    image: 'sunchon.png',
+  },
+  {
+    name: '순천향대학교',
+    domain: '@sch.ac.kr',
+    image: 'sch.png',
+  },
+  {
+    name: '숭실대학교',
+    domain: '@ssu.ac.kr',
+    image: 'ssu.png',
+  },
+  {
+    name: '신경대학교',
+    domain: '@sgu.ac.kr',
+    image: 'sgu.png',
+  },
+  {
+    name: '신라대학교',
+    domain: '@silla.ac.kr',
+    image: 'silla.png',
+  },
+  {
+    name: '신한대학교',
+    domain: '@shinhan.ac.kr',
+    image: 'shinhan.png',
+  },
+  {
+    name: '아세아연합신학대학교',
+    domain: '@acts.ac.kr',
+    image: 'acts.png',
+  },
+  {
+    name: '아주대학교',
+    domain: '@ajou.ac.kr',
+    image: 'ajou.png',
+  },
+  {
+    name: '안동대학교',
+    domain: '@andong.ac.kr',
+    image: 'andong.png',
+  },
+  {
+    name: '안양대학교',
+    domain: '@anyang.ac.kr',
+    image: 'anyang.png',
+  },
+  {
+    name: '연세대학교',
+    domain: '@yonsei.ac.kr',
+    image: 'yonsei.png',
+  },
+  {
+    name: '영남대학교',
+    domain: '@yu.ac.kr',
+    image: 'yu.png',
+  },
+  {
+    name: '영남신학대학교',
+    domain: '@ytus.ac.kr',
+    image: 'ytus.png',
+  },
+  {
+    name: '영산대학교',
+    domain: '@ysu.ac.kr',
+    image: 'ysu.png',
+  },
+  {
+    name: '영산선학대학교',
+    domain: '@youngsan.ac.kr',
+    image: 'youngsan.png',
+  },
+  {
+    name: '예수대학교',
+    domain: '@jesus.ac.kr',
+    image: 'jesus.png',
+  },
+  {
+    name: '예원예술대학교',
+    domain: '@yewon.ac.kr',
+    image: 'yewon.png',
+  },
+  {
+    name: '용인대학교',
+    domain: '@yongin.ac.kr',
+    image: 'yongin.png',
+  },
+  {
+    name: '우석대학교',
+    domain: '@woosuk.ac.kr',
+    image: 'woosuk.png',
+  },
+  {
+    name: '우송대학교',
+    domain: '@wsu.ac.kr',
+    image: 'wsu.png',
+  },
+  {
+    name: '울산과학기술원',
+    domain: '@unist.ac.kr',
+    image: 'unist.png',
+  },
+  {
+    name: '울산대학교',
+    domain: '@ulsan.ac.kr',
+    image: 'ulsan.png',
+  },
+  {
+    name: '원광대학교',
+    domain: '@wonkwang.ac.kr',
+    image: 'wonkwang.png',
+  },
+  {
+    name: '위덕대학교',
+    domain: '@uu.ac.kr',
+    image: 'uu.png',
+  },
+  {
+    name: '유원대학교',
+    domain: '@u1.ac.kr',
+    image: 'u1.png',
+  },
+  {
+    name: '을지대학교',
+    domain: '@eulji.ac.kr',
+    image: 'eulji.png',
+  },
+  {
+    name: '이화여자대학교',
+    domain: '@ewha.ac.kr',
+    image: 'ewha.png',
+  },
+  {
+    name: '인제대학교',
+    domain: '@inje.ac.kr',
+    image: 'inje.png',
+  },
+  {
+    name: '인천가톨릭대학교',
+    domain: '@iccu.ac.kr',
+    image: 'iccu.png',
+  },
+  {
+    name: '인천대학교',
+    domain: '@inu.ac.kr',
+    image: 'inu.png',
+  },
+  {
+    name: '인하대학교',
+    domain: '@inha.ac.kr',
+    image: 'inha.png',
+  },
+  {
+    name: '장로회신학대학교',
+    domain: '@puts.ac.kr',
+    image: 'puts.png',
+  },
+  {
+    name: '전남대학교',
+    domain: '@jnu.ac.kr',
+    image: 'jnu.png',
+  },
+  {
+    name: '전북대학교',
+    domain: '@jbnu.ac.kr',
+    image: 'jbnu.png',
+  },
+  {
+    name: '전주대학교',
+    domain: '@jj.ac.kr',
+    image: 'jj.png',
+  },
+
+  {
+    name: '제주국제대학교',
+    domain: '@jeju.ac.kr',
+    image: 'jeju.png',
+  },
+  {
+    name: '제주대학교',
+    domain: '@jejunu.ac.kr',
+    image: 'jejunu.png',
+  },
+  {
+    name: '조선대학교',
+    domain: '@chosun.ac.kr',
+    image: 'chosun.png',
+  },
+  {
+    name: '중부대학교',
+    domain: '@joongbu.ac.kr',
+    image: 'joongbu.png',
+  },
+  {
+    name: '중앙대학교',
+    domain: '@cau.ac.kr',
+    image: 'cau.png',
+  },
+  {
+    name: '중앙승가대학교',
+    domain: '@admissionsangha.ac.kr',
+    image: 'admissionsangha.png',
+  },
+  {
+    name: '중원대학교',
+    domain: '@jwu.ac.kr',
+    image: 'jwu.png',
+  },
+  {
+    name: '차의과학대학교',
+    domain: '@cha.ac.kr',
+    image: 'cha.png',
+  },
+  {
+    name: '창신대학교',
+    domain: '@cs.ac.kr',
+    image: 'cs.png',
+  },
+  {
+    name: '창원대학교',
+    domain: '@changwon.ac.kr',
+    image: 'changwon.png',
+  },
+  {
+    name: '청주대학교',
+    domain: '@cju.ac.kr',
+    image: 'cju.png',
+  },
+  {
+    name: '초당대학교',
+    domain: '@cdu.ac.kr',
+    image: 'cdu.png',
+  },
+  {
+    name: '총신대학교',
+    domain: '@chongshin.ac.kr',
+    image: 'chongshin.png',
+  },
+  {
+    name: '추계예술대학교',
+    domain: '@chugye.ac.kr',
+    image: 'chugye.png',
+  },
+  {
+    name: '충남대학교',
+    domain: '@cnu.ac.kr',
+    image: 'cnu.png',
+  },
+  {
+    name: '충북대학교',
+    domain: '@chungbuk.ac.kr',
+    image: 'chungbuk.png',
+  },
+  {
+    name: '칼빈대학교',
+    domain: '@calvin.ac.kr',
+    image: 'calvin.png',
+  },
+  {
+    name: '케이씨대학교',
+    domain: '@kcu.ac.kr',
+    image: 'kcu.png',
+  },
+  {
+    name: '평택대학교',
+    domain: '@ptu.ac.kr',
+    image: 'ptu.png',
+  },
+  {
+    name: '포항공과대학교',
+    domain: '@postech.ac.kr',
+    image: 'postech.png',
+  },
+  {
+    name: '한경대학교',
+    domain: '@hknu.ac.kr',
+    image: 'hknu.png',
+  },
+  {
+    name: '한국과학기술원',
+    domain: '@kaist.ac.kr',
+    image: 'kaist.png',
+  },
+  {
+    name: '한국교원대학교',
+    domain: '@knue.ac.kr',
+    image: 'knue.png',
+  },
+  {
+    name: '한국교통대학교',
+    domain: '@ut.ac.kr',
+    image: 'ut.png',
+  },
+  {
+    name: '한국국제대학교',
+    domain: '@iuk.ac.kr',
+    image: 'iuk.png',
+  },
+  {
+    name: '한국기술교육대학교',
+    domain: '@koreatech.ac.kr',
+    image: 'koreatech.png',
+  },
+  {
+    name: '한국산업기술대학교',
+    domain: '@kpu.ac.kr',
+    image: 'kpu.png',
+  },
+  {
+    name: '한국성서대학교',
+    domain: '@bible.ac.kr',
+    image: 'bible.png',
+  },
+  {
+    name: '한국대학외국어대학교',
+    domain: '@hufs.ac.kr',
+    image: 'hufs.png',
+  },
+  {
+    name: '한국전통문화대학교',
+    domain: '@nuch.ac.kr',
+    image: 'nuch.png',
+  },
+  {
+    name: '한국체육대학교',
+    domain: '@knsu.ac.kr',
+    image: 'knsu.png',
+  },
+  {
+    name: '한국침례신학대학교',
+    domain: '@kbtus.ac.kr',
+    image: 'kbtus.png',
+  },
+  {
+    name: '한국항공대학교',
+    domain: '@kau.ac.kr',
+    image: 'kau.png',
+  },
+  {
+    name: '한국해양대학교',
+    domain: '@kmou.ac.kr',
+    image: 'kmou.png',
+  },
+  {
+    name: '한남대학교',
+    domain: '@hannam.ac.kr',
+    image: 'hannam.png',
+  },
+  {
+    name: '한동대학교',
+    domain: '@handongedu.ac.kr',
+    image: 'handongedu.png',
+  },
+  {
+    name: '한라대학교',
+    domain: '@halla.ac.kr',
+    image: 'halla.png',
+  },
+  {
+    name: '한려대학교',
+    domain: '@hanlyo.ac.kr',
+    image: 'hanlyo.png',
+  },
+  {
+    name: '한밭대학교',
+    domain: '@hanbat.ac.kr',
+    image: 'hanbat.png',
+  },
+  {
+    name: '한서대학교',
+    domain: '@hanseo.ac.kr',
+    image: 'hanseo.png',
+  },
+  {
+    name: '한성대학교',
+    domain: '@hansung.ac.kr',
+    image: 'hansung.png',
+  },
+  {
+    name: '한세대학교',
+    domain: '@hansei.ac.kr',
+    image: 'hansei.png',
+  },
+  {
+    name: '한신대학교',
+    domain: '@hs.ac.kr',
+    image: 'hs.png',
+  },
+  {
+    name: '한양대학교',
+    domain: '@hanyang.ac.kr',
+    image: 'hanyang.png',
+  },
+  {
+    name: '한일장신대학교',
+    domain: '@hanil.ac.kr',
+    image: 'hanil.png',
+  },
+  {
+    name: '협성대학교',
+    domain: '@uhs.ac.kr',
+    image: 'uhs.png',
+  },
+  {
+    name: '호남대학교',
+    domain: '@honam.ac.kr',
+    image: 'honam.png',
+  },
+  {
+    name: '호남신학대학교',
+    domain: '@htus.ac.kr',
+    image: 'htus.png',
+  },
+  {
+    name: '호서대학교',
+    domain: '@hoseo.ac.kr',
+    image: 'hoseo.png',
+  },
+  {
+    name: '홍익대학교',
+    domain: '@hongik.ac.kr',
+    image: 'hongik.png',
+  },
+  {
+    name: '홍익대학교세종캠퍼스',
+    domain: '@sejonghongik.ac.kr',
+    image: 'sejonghongik.png',
   },
 ]
