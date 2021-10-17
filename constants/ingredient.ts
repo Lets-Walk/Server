e2d546b31720735d9e1d2a9924713c3b2d9c28d8
jjonyo <jonghyuk96@cau.ac.kr>
2021-10-18 03:03:55 +0900

    Fix typo

--- constants/ingredient.ts
+++ constants/ingredient.ts
@@ -2,7 +2,7 @@ export default {
   공과대학: ['Pencil', 'Computer'],
   자연과학대학: ['Pencil', 'Microscope'],
   인문대학: ['Pencil', 'Book'],
-  경역대학: ['Pencil', 'Calculator'],
+  경영대학: ['Pencil', 'Calculator'],
   의과대학: ['Pencil', 'Stethoscope'],
   약학대학: ['Pencil', 'Pill'],
 }
